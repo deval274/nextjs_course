a8fb3058b2ed82baaecbbe8cb90a8f5bde999dd9
Deval_Ujeniya <devalujeniya@gmail.com>
2026-06-08 20:31:33 +0530

    06_custom_hook: feat: update App component to display products and integrate Cart functionality using useCart hook

--- 06_custom_hook/src/App.jsx
+++ 06_custom_hook/src/App.jsx
@@ -1,10 +1,37 @@
 import "./App.css";
+import { useCart } from "./hooks/useCart";
+import { products } from "./data/products";
+import ProductCard from "./components/ProductCard";
+import Cart from "./components/Cart";
 
 function App() {
+  const { cart, addToCart, removeFromCart, updateQuantity, total } = useCart();
   return (
     <>
-      <div></div>
-      <h1>Custom Hook</h1>
+      <div className="app">
+        <header>
+          <h1>Shopping Cart</h1>
+        </header>
+        <main className="products">
+          <section>
+            {products.map((product) => (
+              <ProductCard
+                key={product.id}
+                product={product}
+                onAddToCart={addToCart}
+              />
+            ))}
+          </section>
+          <section>
+            <Cart
+              cart={cart}
+              onUpdateQuantity={updateQuantity}
+              onRemoveFromCart={removeFromCart}
+              total={total}
+            />
+          </section>
+        </main>
+      </div>
     </>
   );
 }
